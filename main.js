122db92a5353acc53caa25b984a1384e109335bf
Gregory.Johnson <3531op@gmail.com>
2020-12-20 17:33:20 -0500

    Working on feature to delete individual objects

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ if (db === null || db === undefined) {
 }
 const dataBase = document.querySelector('#submit-button')
 dataBase.addEventListener(`click`, (event) => {
-  // console.log(event)
 
   const domainName = document.querySelector('#domain-name')
   domain = domainName.value
-
-  localStorage.clear(domain)
-  console.log(domainName.value)
+  domainName.value = ''
 
   const userName = document.querySelector('#username')
   username = userName.value
+  userName.value = ''
 
   const pass = document.querySelector('#password')
   password = pass.value
+  pass.value = ''
+
+  const deleteButton = document.querySelector('#delete-all-button')
+  deleted = deleteButton.value
 
   const domainLI = document.createElement('li')
   domainLI.innerText = ` Domain Name: ${domainName.value}`
@@ -27,22 +29,23 @@ dataBase.addEventListener(`click`, (event) => {
   const passwordLI = document.createElement('li')
   passwordLI.innerText = `Password: ${pass.value}`
 
+  const deleteButtonLI = document.createElement('button')
+  deleteButtonLI.innerText = "delete"
+
   const ul = document.querySelector('ul')
   ul.appendChild(domainLI)
   ul.appendChild(usernameLI)
   ul.appendChild(passwordLI)
+  ul.appendChild(deleteButtonLI)
 
   const newDB = {
     domain: domain,
     username: username,
     password: password,
+    button: deleteButtonLI, 
   }
   db.push(newDB)
   localStorage.setItem("db", JSON.stringify(db))
-
-  window.location.reload()
-
-
 })
 
 const ul = document.querySelector('ul')
@@ -58,16 +61,21 @@ for (const newObj of db) {
   const pass = document.createElement('li')
   pass.innerText = `Password: ${newObj.password}`
 
+  const byeBye = document.createElement('button') 
+
+
+
   ul.appendChild(domainName)
   ul.appendChild(userName)
   ul.appendChild(pass)
+  ul.appendChild(byeBye)
 
 }
 
 
 
 const deleteAll = document.querySelector('#delete-all-button')
-deleteAll.addEventListener('keydown', (event) => {
+deleteAll.addEventListener('keydown', () => {
   localStorage.clear();
   window.location.reload()
 })
