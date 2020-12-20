e5848ceee7650f617a597ea36e2e1a307981d220
Gregory.Johnson <3531op@gmail.com>
2020-12-19 19:25:59 -0500

    Got local storage saving user data

--- main.js
+++ main.js
@@ -1,26 +1,66 @@
-const usrInput = document.querySelector('#domain-name')
+let db = JSON.parse(localStorage.getItem("db"))
+if (db === null || db === undefined) {
+  db = []
+}
 const dataBase = document.querySelector('#new-button')
 dataBase.addEventListener(`click`, () => {
 
-  const li = document.createElement('li')
-  li.innerText = input.value;
+  const domainName = document.querySelector('#domain-name')
+  domain = domainName.value
+
+  const userName = document.querySelector('#username')
+  username = userName.value
+
+  const pass = document.querySelector('#password')
+  password = pass.value
+
+  const domainLI = document.createElement('li')
+  domainLI.innerText = ` Domain Name: ${domainName.value}`
+
+  const usernameLI = document.createElement('li')
+  usernameLI.innerText = `Username: ${userName.value}`
+
+  const passwordLI = document.createElement('li')
+  passwordLI.innerText = `Password: ${pass.value}`
+
   const ul = document.querySelector('ul')
-  ul.appendChild(li)
+  ul.appendChild(domainLI)
+  ul.appendChild(usernameLI)
+  ul.appendChild(passwordLI)
 
-  
+  const newDB = {
+    domain: domain,
+    username: username,
+    password: password,
+  }
+  db.push(newDB)
+  localStorage.setItem("db", JSON.stringify(db))
 
-const domainName = document.querySelector('#domain-name')
-domain = domainName.value  
-db.push(domain)
-
-const userName = document.querySelector('#username')
-user = userName.value
-db.push(user)
-
-const pass = document.querySelector('#username')
-password = pass.value
-db.push(password)
 
 })
 
+const ul = document.querySelector('ul')
 
+for (const newObj of db) {
+
+  const domainName = document.createElement('li')
+  domainName.innerText = ` Domain Name: ${newObj.domain}`
+
+  const userName = document.createElement('li')
+  userName.innerText = `Username: ${newObj.username}`
+
+  const pass = document.createElement('li')
+  pass.innerText = `Password: ${newObj.password}`
+
+  ul.appendChild(domainName)
+  ul.appendChild(userName)
+  ul.appendChild(pass)
+
+}
+
+
+
+const deleteAll = document.querySelector('#delete-all-button')
+deleteAll.addEventListener('click', () => {
+localStorage.clear()
+})
